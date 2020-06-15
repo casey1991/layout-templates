75da509588378a20ce4e950405172da7133488a8
Casey Wang <caseywang@httpeace.com>
2020-06-15 21:55:06 +0800

    #LAYOUT: fix document issue

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,13 +1,28 @@
-import { Html, Head, Main, NextScript } from "next/document";
-const Document = () => {
-  return (
-    <Html>
-      <Head />
-      <body>
-        <Main />
-        <NextScript />
-      </body>
-    </Html>
-  );
-};
-export default Document;
+import Document, {
+  Html,
+  Head,
+  Main,
+  NextScript,
+  DocumentContext,
+} from "next/document";
+
+class CustomDocument extends Document {
+  static async getInitialProps(ctx: DocumentContext) {
+    const initialProps = await Document.getInitialProps(ctx);
+    return { ...initialProps };
+  }
+
+  render() {
+    return (
+      <Html>
+        <Head />
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}
+
+export default CustomDocument;
